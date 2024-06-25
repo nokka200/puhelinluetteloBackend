4fb247cb6a5a523f077e57a908a15d7dde85298e
Niku <nokka200@gmail.com>
2024-06-25 17:48:36 +0300

    3.9 lisätty cors ja korjattu url

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const cors = require('cors');
 
 
 const PORT = 3001;
@@ -17,6 +18,7 @@ app.use((req, res, next) => {
         morgan('tiny')(req, res, next);
     }
 });
+app.use(cors());
     
     
     
