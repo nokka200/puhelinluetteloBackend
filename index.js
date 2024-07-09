ba1bce3bfb175e41b2641a78d34d904316e31964
Niku <nokka200@gmail.com>
2024-07-09 14:16:17 +0300

    Lisätty middlewarea

--- index.js
+++ index.js
@@ -8,8 +8,11 @@ const Phone = require('./models/phone');
 
 
 
-// Muista middleware!
+// Middleware
+app.use(express.static('dist'));
 app.use(express.json());
+app.use(cors());
+
 morgan.token('post', (req, res) => {
     return JSON.stringify(req.body)
 })
@@ -20,11 +23,6 @@ app.use((req, res, next) => {
         morgan('tiny')(req, res, next);
     }
 });
-app.use(cors());
-app.use(express.static('dist'));
-
-
-//app.use(morgan('tiny'));
 
 let numbers = [
     {
@@ -49,6 +47,7 @@ let numbers = [
     }
 ];
 
+// Http responses
 app.get('/api/persons', (request, response) => {
     Phone.find({}).then(person => {
         response.json(person)
@@ -72,10 +71,10 @@ app.get('/api/persons/:id', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response, next) => {
     Phone.findByIdAndDelete(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 });
 
 app.post('/api/persons', (request, response) => {
@@ -95,6 +94,27 @@ app.post('/api/persons', (request, response) => {
         response.json(savedPhone)
     });
 });
+
+// middlewarea
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    };
+
+    next(error)
+}
+
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
